64a7bc798a99e2cc078db426541718ebe6f252fe
Bread <luphoria@innocent.com>
2022-06-18 23:27:50 -0500

    add comments

--- base.js
+++ base.js
@@ -6,6 +6,7 @@ fs = require("fs");
 const REPEAT_MS = 500;
 const PORT = 1234;
 
+// Init all user data
 let user = {
   cookies: 0,
   store: {
@@ -23,16 +24,20 @@ let user = {
   },
 };
 
+// (TODO) bool which will be used to stop chunk loops once the user quits the game.
 let clientConnectionStopped = false;
 
+// Create the http server
 http
   .createServer((req, res) => {
+    // Specify which endpoints. We cut off the parameters because we don't use them (and the form submit auto-adds them).
     switch (req.url.split("?")[0]) {
       case "/":
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(fs.readFileSync("./index.html"));
         break;
       case "/cookie":
+        // Server endpoint used only to count up the number of cookie clicks.
         res.writeHead(204);
         res.end("");
         user.cookies += 1;
@@ -43,21 +48,27 @@ http
           Connection: "keep-alive",
         });
         res.write(fs.readFileSync("./counter.html"));
+
+        // Set up a loop of chunks, so that we can slowly provide up-to-date information
         let loop = 0;
         const loop_update = () => {
           setTimeout(() => {
+            // Add a chunk of updated HTML, along with extra CSS to hide the outdated HTML
             res.write(
               `<b id="c${loop}">${user.cookies}</b><style>#c${
                 loop - 1
               }{display:none}</style>`
             );
+            // increment loop for next iteration to hide
             loop++;
+            // Re-call the same function [TODO: only if the client still is connected]
             if (!clientConnectionStopped) loop_update();
           }, REPEAT_MS);
         };
         loop_update();
         break;
       default:
+        // just try to grab the file requested. [TODO - replace this with a 404 and its dependents with specified endpoints]
         res.writeHead(200);
         res.end(fs.readFileSync("." + req.url.split("?")[0])) 
         break;
